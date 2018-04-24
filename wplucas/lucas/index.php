3e99042aba57db8fb57df93f00c6737ce31308e6
LUCAS RYUICHI MARTINEZ NAKAGAWA <lucasnakagawa@univille.br>
2018-04-23 21:30:57 -0300

    Segundo trabalho da DACS 2018 do 1 BIM

---
+++ wplucas/lucas/index.php
@@ -0,0 +1,19 @@
+<?php
+global $wpdb;
+
+$query = "SELECT post_title, post_content, post_date FROM $wpdb->posts WHERE post_status = 'publish' ORDER BY post_date DESC";
+$rows = $wpdb->get_results($query);
+foreach($rows as $row) {
+	$postTitle = $row->post_title;
+	$postContent = $row->post_content;
+	$postDate = $row->post_date;
+
+	echo "<h1>" . $postTitle . "</h1>";
+	echo "</br>";
+	echo "<h2>" . $postContent . "</h2>";
+	echo "</br>";
+	echo "<h2>" . $postDate . "</h2>";
+	echo "</br>";
+	echo "<hr>";
+}
+?>
