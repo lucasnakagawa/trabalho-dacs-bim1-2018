0175ce0b3c13c6bfcd20f5efbee90a010960527d
LUCAS RYUICHI MARTINEZ NAKAGAWA <lucasnakagawa@univille.br>
2018-05-02 20:29:38 -0300

    Mudança do layout

--- wplucas/lucas/index.php
+++ wplucas/lucas/index.php
@@ -1,4 +1,5 @@
 <?php
+get_header();
 global $wpdb;
 
 $query = "SELECT post_title, post_content, post_date FROM $wpdb->posts WHERE post_status = 'publish' ORDER BY post_date DESC";
@@ -7,13 +8,14 @@ foreach($rows as $row) {
 	$postTitle = $row->post_title;
 	$postContent = $row->post_content;
 	$postDate = $row->post_date;
-
-	echo "<h1>" . $postTitle . "</h1>";
+	
+	echo "<h1 class='title'>" . $postTitle . "</h1>";
 	echo "</br>";
 	echo "<h2>" . $postContent . "</h2>";
 	echo "</br>";
 	echo "<h2>" . $postDate . "</h2>";
 	echo "</br>";
 	echo "<hr>";
+	echo "</p>";
 }
 ?>
